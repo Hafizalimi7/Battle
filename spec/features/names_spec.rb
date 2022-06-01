437aad876ee4f493b45bd1193f9058748a2f4dd9
Hafiz <hafizalimi77@gmail.com>
2022-06-01 18:43:40 +0100

    Test Helpers completed

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,12 +1,6 @@
 feature "Entering Names" do
     scenario 'Submitting Names' do
-        visit('/')
-        fill_in :player_1_name, with: 'Hafiz'
-        fill_in :player_2_name, with: 'Dave'
-        click_button('Submit')
-
-        # save_and_open_page
-
+        sign_in_and_play
         expect(page).to have_content "Hafiz vs Dave"
     end
 end
